bc1deb5c9db53666133ef5f3a71d1d81e6246ddc
wilson angel <wilson.angel@globant.com>
2021-04-07 18:38:45 -0500

    base calculator with two methods +-

--- src/calculator2021/Calculator.java
+++ src/calculator2021/Calculator.java
@@ -6,6 +6,15 @@ public class Calculator {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+
+	public int sum(int a, int b) {
+		return a+b;			
+
+	}
+
+	public int substract(int a, int b) {
+		return a-b;
+	}
 
 }
